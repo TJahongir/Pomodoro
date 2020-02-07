89c280be853c80110de0964353deb4f31ca9e34f
Jahongir <jakhongirt@gmail.com>
2020-02-07 12:39:27 +0500

    Made a Timer ticking

---
+++ js/main.js
@@ -0,0 +1,53 @@
+const startBtn = document.querySelector('#start');
+const pauseBtn = document.querySelector('#pause');
+const timeDOM = document.querySelector('.counter-time');
+
+let modes = [
+  {
+    time: 1500,
+    shortbreak: 300,
+    longBreak: 900,
+    longBreakDelay: 4
+  },
+  {
+    time: 3000,
+    shortbreak: 600,
+    longBreak: 1200,
+    longBreakDelay: 4
+  }
+];
+
+let defaultMode = modes[0];
+let workMode = modes[1];
+
+// Minute / Seconds Converter
+const formatTime = time => {
+  let minutes = Math.floor(time / 60);
+
+  let seconds = Math.floor(time % 60);
+
+  if (seconds < 10) {
+    seconds = `0${seconds}`;
+  }
+
+  return `${minutes}:${seconds}`;
+};
+
+const startTimer = mode => {
+  let timePassed = 0;
+  let timeLeft = mode.time;
+
+  let timerInterval = setInterval(() => {
+    timePassed += 1;
+    timeLeft = 5 - timePassed;
+    timeDOM.textContent = formatTime(timeLeft);
+
+    if (timeLeft == 0) {
+      clearInterval(timerInterval);
+    }
+  }, 1000);
+};
+
+startBtn.addEventListener('click', () => {
+  startTimer(defaultMode);
+});
